3e44b0df27992bbb63eacbe3a3d7d04a45062265
Adrian <adrian.cajamarca.est@tecazuay.edu.ec>
2022-09-19 17:05:12 -0500

    78

--- src/Control/controladminVistas.java
+++ src/Control/controladminVistas.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Control;
 
-import Modelo.PersonaAsignaturaJpaController;
 import Modelo.PersonaJpaController;
 import Vista.Administrador.viewCrearAdmin;
 import Vista.Subvista.viewCuestionarios;
